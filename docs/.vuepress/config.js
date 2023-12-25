8c7f5d4cc2b0a6d1ae94e89b950cf394862a8068
luluxiaoyu <ljy.xiaoyu@qq.com>
2023-12-25 21:39:23 +0800

    test

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    title: 'NSTARMC群服务器文档',
+    description: '一个简单的文档',
+    themeConfig: {
+      sidebar: 'auto',
+      logo: '/hero.png',
+      nav: [
+        { text: 'Doc', link: '/' },
+        { text: 'NSTARMC', link: 'https://www.nstarmc.cn' },
+      ],
+      sidebar: [
+        "/",
+        {
+          title: '加入服务器指南',   // 必要的
+          path: '/Play/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+          collapsable: false, // 可选的, 默认值是 true,
+          sidebarDepth: 1,    // 可选的, 默认值是 1
+          children: [
+            '/'
+          ]
+        }
+      ]
+    }
+  }
---
+++ docs/README.md
@@ -0,0 +1,18 @@
+```yaml
+---
+home: true
+heroImage: /hero.png
+heroText: NSTARMC群服务器文档
+tagline: 一个简单的文档
+actionText: 立刻阅读 →
+actionLink: /play/
+features:
+- title: 快速上手
+  details: 定制的MC客户端，一个客户端解决全部问题，傻瓜式操作。
+- title: 公益
+  details: 不搞商业，只允许赞助。
+- title: 一起玩~
+  details: 就是开着群里的人一起玩的而已啦！
+footer: MIT Licensed | Copyright © 2020-2024 NSTARMC
+---
+```
